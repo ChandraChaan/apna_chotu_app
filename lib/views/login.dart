f770690d6ee85177a98b664a7e18f7cebeea82d1
Chandra <dchandrachaan@gmail.com>
2023-08-11 08:12:24 +0530

    naming conversion, and common widget created

---
+++ lib/views/login.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+
+import 'common/linear_background.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  List<String> countryCodes = ['+1', '+44', '+91', '+86'];
+  String selectedCountryCode = '+91';
+  TextEditingController phoneNumberController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return LinearBackground(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          'Apna Chotu Customer Login',
+          style: Theme.of(context)
+              .textTheme
+              .displaySmall
+              ?.copyWith(color: Colors.white, fontWeight: FontWeight.w400),
+        ),
+        const SizedBox(height: 8),
+        Text(
+          'Login access is available to customers who have registered.',
+          style: Theme.of(context).textTheme.titleMedium?.copyWith(
+              color: Colors.white, fontSize: 16, fontWeight: FontWeight.w400),
+        ),
+        const SizedBox(height: 16),
+        Image.asset('assets/images/fingerprint.png'),
+        const SizedBox(height: 16),
+        Text(
+          'Enter registered mobile number',
+          style: Theme.of(context).textTheme.titleMedium?.copyWith(
+              color: Colors.white, fontSize: 16, fontWeight: FontWeight.w400),
+        ),
+        const SizedBox(height: 8),
+        Row(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: DropdownButton<String>(
+                value: selectedCountryCode,
+                onChanged: (newValue) {
+                  setState(() {
+                    selectedCountryCode = newValue!;
+                  });
+                },
+                items: countryCodes.map((code) {
+                  return DropdownMenuItem<String>(
+                    value: code,
+                    child: Text(code),
+                  );
+                }).toList(),
+              ),
+            ),
+            const SizedBox(width: 10),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextFormField(
+                  controller: phoneNumberController,
+                  keyboardType: TextInputType.phone,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter your phone number',
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.all(12),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 16),
+        SizedBox(
+          width: double.infinity,
+          height: 50,
+          child: ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              shape: const StadiumBorder(),
+              backgroundColor: const Color(0xFF6724),
+            ),
+            child: Text(
+              'Login via OTP',
+              style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                  color: Colors.white,
+                  fontSize: 21,
+                  fontWeight: FontWeight.w700),
+            ),
+          ),
+        ),
+        const SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Not have an account? ',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                // Handle create account action here
+              },
+              child: const Text(
+                'Create Now',
+                style: TextStyle(
+                  color: Colors.white,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    ));
+  }
+}
