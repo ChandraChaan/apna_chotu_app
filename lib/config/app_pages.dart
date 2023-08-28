f05b1891da2d07bffafc9803db1ec1c24e0571db
Chandra <dchandrachaan@gmail.com>
2023-08-28 21:25:00 +0530

    slider changes

--- lib/config/app_pages.dart
+++ lib/config/app_pages.dart
@@ -17,7 +17,7 @@ import '../presentation/select_location_screen.dart';
 part 'app_route.dart';
 
 class AppPages {
-  static const initial = Routes.dashBoardListScreen;
+  static const initial = Routes.intro;
   static final routes = [
     GetPage(
         name: Routes.intro,
