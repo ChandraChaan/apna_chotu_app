06fb50525dc1267d6f8f81e2687fa74c6d666246
Chandra <dchandrachaan@gmail.com>
2023-08-30 17:19:51 +0530

    map screen changes

--- lib/presentation/locations_screens/location_screen.dart
+++ lib/presentation/locations_screens/location_screen.dart
@@ -5,6 +5,7 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_search_bar/flutter_search_bar.dart';
 
 import '../../Config/app_pages.dart';
 import '../../utils/rounded_button.dart';
@@ -19,12 +20,15 @@ class MapScreen extends StatefulWidget {
 class _MapScreenState extends State<MapScreen> {
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
+  final Set<Marker> markers = {};
 
   String address = '';
   String locality = '';
   String streetName = '';
   double latitude = 17.3850;
   double longitude = 78.4867;
+  double selectedLatitude = 0.0; // Initialize to some default value
+  double selectedLongitude = 0.0; // Initialize to some default value
 
   static const CameraPosition initialCameraPosition = CameraPosition(
     target: LatLng(37.43296265331129, -122.08832357078792),
@@ -69,6 +73,17 @@ class _MapScreenState extends State<MapScreen> {
           latitude = position.latitude;
           longitude = position.longitude;
         });
+
+        // Add a marker for the user's live location
+        markers.add(
+          Marker(
+            markerId: MarkerId('user_location'),
+            position: LatLng(latitude, longitude),
+            infoWindow: InfoWindow(title: 'Your Location'),
+            icon: BitmapDescriptor.defaultMarkerWithHue(
+                BitmapDescriptor.hueOrange),
+          ),
+        );
 
         _newPosition();
       } else {
@@ -139,40 +154,132 @@ class _MapScreenState extends State<MapScreen> {
       body: Stack(
         children: [
           GoogleMap(
+            compassEnabled:false,
             mapType: MapType.normal,
             initialCameraPosition: initialCameraPosition,
             onMapCreated: (GoogleMapController controller) {
               _controller.complete(controller);
             },
+            markers: markers, // Display the markers on the map
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            child: Container(
+              width: 100,
+              padding: const EdgeInsets.all(10),
+              color: Colors.white,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search for area, street name',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  prefixIcon: const Icon(
+                    Icons.search,
+                    size: 30,
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 16.0),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 160,
+            child: Center(
+              // Center the button horizontally
+              child: GestureDetector(
+                onTap: _getLocation,
+                child: Container(
+                  width: 200, // Set a fixed width
+                  height: 50, // Set a fixed height
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(25),
+                    border: Border.all(
+                      color: Colors.deepOrange,
+                    ),
+                    // Add rounded corners
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.my_location, color: Colors.deepOrange),
+                      SizedBox(width: 5),
+                      Text(
+                        'Use Current Location',
+                        style: TextStyle(
+                          color: Colors.deepOrange, // Text color
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ),
           Positioned(
             left: 0,
             right: 0,
             bottom: 0,
             child: Container(
-              height: 90,
-              padding: const EdgeInsets.all(16.0),
+              height: 140,
               color: Colors.white,
-              child: RoundedButton(
-                onPressed: address.isNotEmpty
-                    ? () {
-                        Get.toNamed(
-                          Routes.othersAddress,
-                          arguments: {
-                            'latitude': latitude,
-                            'longitude': longitude,
-                            'address': address,
-                            'locality': locality,
-                            'street': streetName,
-                          },
-                        );
-                        print('the address was $address');
-                      }
-                    : null,
-                name: 'Enter complete address',
+              padding: EdgeInsets.only(left: 8.0, right: 8),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ListTile(
+                    horizontalTitleGap: 0,
+                    minVerticalPadding: 0,
+                    contentPadding: EdgeInsets.all(0.0),
+                    leading: Icon(
+                      Icons.location_pin,
+                      size: 35,
+                      color: Colors.deepOrange,
+                    ),
+                    title: Text(locality),
+                    subtitle: Text(
+                      address,
+                      maxLines: 1,
+                    ),
+                  ),
+                  RoundedButton(
+                    width: MediaQuery.of(context).size.width / 1.2,
+                    height: 45,
+                    onPressed: address.isNotEmpty
+                        ? () {
+                            Get.toNamed(
+                              Routes.othersAddress,
+                              arguments: {
+                                'latitude': selectedLatitude,
+                                'longitude': selectedLongitude,
+                                'address': address,
+                                'locality': locality,
+                                'street': streetName,
+                              },
+                            );
+                            print('the address was $address');
+                          }
+                        : null,
+                    name: 'Enter complete address',
+                  ),
+                ],
               ),
             ),
-          )
+          ),
         ],
       ),
     );
