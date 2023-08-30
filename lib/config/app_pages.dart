9f8a852fe414101ffbc65920eea5e28ea494284a
Chandra <dchandrachaan@gmail.com>
2023-08-30 23:37:57 +0530

    map screen changes

--- lib/config/app_pages.dart
+++ lib/config/app_pages.dart
@@ -17,7 +17,7 @@ import '../presentation/locations_screens/select_location_screen.dart';
 part 'app_route.dart';
 
 class AppPages {
-  static const initial = Routes.intro;
+  static const initial = Routes.mapScreen;
   static final routes = [
     GetPage(
         name: Routes.intro,
