be0a7171b3d44161864e5c530af3db7ef7136d11
satya <sathyayadav27@gmail.com>
2023-08-31 10:21:11 +0530

    restaurantDetails

--- lib/config/app_pages.dart
+++ lib/config/app_pages.dart
@@ -5,6 +5,7 @@ import 'package:apna_chotu_app/presentation/locations_screens/address_new_other_
 import 'package:apna_chotu_app/presentation/restuarant/non_veg_closesoon.dart';
 import 'package:apna_chotu_app/presentation/restuarant/non_veg_menu.dart';
 import 'package:apna_chotu_app/presentation/otp_screen.dart';
+import 'package:apna_chotu_app/presentation/restuarant/restuarant_details.dart';
 import 'package:apna_chotu_app/presentation/restuarant/veg_menu.dart';
 import 'package:get/get_navigation/src/routes/get_route.dart';
 
@@ -17,7 +18,7 @@ import '../presentation/locations_screens/select_location_screen.dart';
 part 'app_route.dart';
 
 class AppPages {
-  static const initial = Routes.intro;
+  static const initial = Routes.restaurantDetails;
   static final routes = [
     GetPage(
         name: Routes.intro,
@@ -64,6 +65,10 @@ class AppPages {
     GetPage(
         name: Routes.nonVegCloseSoon,
         page: () => NonVegCloseSoon(),
+        binding: MainBinding()),
+    GetPage(
+        name: Routes.restaurantDetails,
+        page: () => RestaurantDetails(),
         binding: MainBinding()),
   ];
 }
